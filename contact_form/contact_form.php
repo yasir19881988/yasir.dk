cfa5ca4fe42672604c2e07353aeaf4670f4a516b
yasir19881988 <yasir19881988@gmail.com>
2026-05-20 20:47:23 +0200

    New

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -8,77 +8,37 @@ $fields = array('name' => 'Name', 'email' => 'Email', 'subject' => 'Subject', 'm
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
-// let's do the sending
+// send form without reCAPTCHA
+try {
+    $senderEmail = filter_var($_POST['email'] ?? '', FILTER_VALIDATE_EMAIL);
+    $mailFrom = $senderEmail ? $senderEmail : $from;
+    $replyTo = $senderEmail ? $senderEmail : $from;
 
-if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])):
-    //your site secret key
-    $secret = '6LdqmCAUAAAAANONcPUkgVpTSGGqm60cabVMVaON';
-    //get verify response data
-
-    $c = curl_init('https://www.google.com/recaptcha/api/siteverify?secret='.$secret.'&response='.$_POST['g-recaptcha-response']);
-    curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-    $verifyResponse = curl_exec($c);
-
-    $responseData = json_decode($verifyResponse);
-    if($responseData->success):
-
-        try
-        {
-            $senderEmail = filter_var($_POST['email'] ?? '', FILTER_VALIDATE_EMAIL);
-            $mailFrom = $senderEmail ? $senderEmail : $from;
-            $replyTo = $senderEmail ? $senderEmail : $from;
-
-            $emailText = nl2br("You have new message from Contact Form\n");
-
-            foreach ($_POST as $key => $value) {
-
-                if (isset($fields[$key])) {
-                    $emailText .= nl2br("$fields[$key]: $value\n");
-                }
-            }
-
-            $headers = array('Content-Type: text/html; charset="UTF-8";',
-                'From: ' . $mailFrom,
-                'Reply-To: ' . $replyTo,
-                'Return-Path: ' . $from,
-            );
-            
-            mail($sendTo, $subject, $emailText, implode("\n", $headers));
-
-            $responseArray = array('type' => 'success', 'message' => $okMessage);
+    $emailText = nl2br("You have new message from Contact Form\n");
+    foreach ($fields as $key => $label) {
+        $value = trim((string)($_POST[$key] ?? ''));
+        if ($value !== '') {
+            $emailText .= nl2br($label . ': ' . $value . "\n");
         }
-        catch (\Exception $e)
-        {
-            $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-        }
+    }
 
-        if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-            $encoded = json_encode($responseArray);
+    $headers = array(
+        'Content-Type: text/html; charset="UTF-8";',
+        'From: ' . $mailFrom,
+        'Reply-To: ' . $replyTo,
+        'Return-Path: ' . $from,
+    );
 
-            header('Content-Type: application/json');
-
-            echo $encoded;
-        }
-        else {
-            echo $responseArray['message'];
-        }
-
-    else:
-        $errorMessage = 'Robot verification failed, please try again.';
-        $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-        $encoded = json_encode($responseArray);
-
-            header('Content-Type: application/json');
-
-            echo $encoded;
-    endif;
-else:
-    $errorMessage = 'Please click on the reCAPTCHA box.';
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+} catch (\Throwable $e) {
     $responseArray = array('type' => 'danger', 'message' => $errorMessage);
-    $encoded = json_encode($responseArray);
+}
 
-            header('Content-Type: application/json');
-
-            echo $encoded;
-endif;
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    header('Content-Type: application/json');
+    echo json_encode($responseArray);
+} else {
+    echo $responseArray['message'];
+}
 
